cc906a0baf1421c0ec814a1449911a6ab0623f0c
cleophas.mashiri <cleophas.mashiri.mtn.com>
2022-04-23 12:53:18 +0200

    State

--- src/main/java/com/behavioral/StateDP.java
+++ src/main/java/com/behavioral/StateDP.java
@@ -1,7 +1,62 @@
 public class StateDP {
 
     public static void main(String[] args) {
-        String s = "State is behavioral design pattern";
+        String s = "State is behavioral design pattern that lets an object change its behavior when its state changes";
         System.out.printf("%s%n", s);
     }
+
+   
 }
+
+ enum EnumState {
+        DRAFT,
+        MODERATION,
+        PUBLISHED
+    }
+
+    // original 
+    class Document {
+        private EnumState state;
+        public void publish() {
+            switch (state) {
+                case DRAFT:
+                    state = EnumState.MODERATION;
+                    break;
+                case MODERATION:
+                    state = EnumState.PUBLISHED;
+                    break;
+                case PUBLISHED:
+                    // do nothing
+                    break;
+            }
+        }
+    }
+
+    class DocumentWithState {
+        public State state;
+        public State render() {
+            state.render();
+        }
+        public State publish() {
+            state.publish();
+        }
+        public void changeState(State state) {
+            this.state = state;
+        }
+    }
+
+    interface State {
+        public abstract State render();
+        public abstract State publish();
+    }
+
+    class Draft implements State {
+        @Override
+        public State render() {
+           
+        }
+        @Override
+        public State publish() {
+           
+        }
+    }
