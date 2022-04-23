34c24bc10028be4c30ac55bff9b825fad678c2f5
cleophas <cleophasmashiri@gmail.com>
2022-04-23 10:57:43 +0200

    update

---
+++ src/main/java/com/behavioral/ChainOfResponsibility.java
@@ -0,0 +1,7 @@
+public class ChainOfReponsibility {
+
+    public static void main(String[] args) {
+        String s = "Chain of reponsibility is behavioral design pattern ";
+        System.out.printf("%s%n", s);
+    }
+}
