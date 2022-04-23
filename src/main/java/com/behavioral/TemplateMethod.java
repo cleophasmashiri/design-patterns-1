b191abbefdac2e7030245363d4b3bb84ee49e322
cleophas <cleophasmashiri@gmail.com>
2022-04-23 10:53:36 +0200

    update

--- src/main/java/com/behavioral/TemplateMethod.java
+++ src/main/java/com/behavioral/TemplateMethod.java
@@ -3,9 +3,15 @@ public class TemplateMethod {
     public static void main(String[] args) {
         String s = "Template method is a behavioral design pattern that defines a general algorithm in a super class and lets subclasses define the step methods"; 
         System.out.printf("%s%n", s);
+
+        Game football = new FootballGame();
+        football.initialise();
     }
 
-    abstract class Game {
+    
+} 
+
+abstract class Game {
 
         public final void initialise() {
             start();
@@ -38,4 +44,3 @@ public class TemplateMethod {
          }
 
     }
-} 
