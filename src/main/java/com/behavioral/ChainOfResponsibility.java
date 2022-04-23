8a5f797bcbe2eb61cbf6d016c98811f38a54d2df
cleophas <cleophasmashiri@gmail.com>
2022-04-23 11:00:34 +0200

    update

--- src/main/java/com/behavioral/ChainOfResponsibility.java
+++ src/main/java/com/behavioral/ChainOfResponsibility.java
@@ -1,7 +1,7 @@
 public class ChainOfReponsibility {
 
     public static void main(String[] args) {
-        String s = "Chain of reponsibility is behavioral design pattern ";
+        String s = "Chain of reponsibility is behavioral design pattern tghhat lets you pass a request through a series of handler methods and each handler can either process or pass it the next handler";
         System.out.printf("%s%n", s);
     }
 }
